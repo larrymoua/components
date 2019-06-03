91485fcd71dee30b657814bbe2762240734ae81c
larrymoua <46528875+larrymoua@users.noreply.github.com>
2019-06-03 18:43:02 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import faker from 'faker';
+import CommentDetail from './CommentDetail';
+import ApprovalCard from './ApprovalCard';
+
+const App = () => {
+  return (
+    <div className="ui container comments">
+    <ApprovalCard>
+      <div>
+      <h4>Warning!</h4>
+      Are you sure you want to do this?
+      </div>
+    </ApprovalCard>
+    <ApprovalCard>
+      <CommentDetail
+        author="Sam"
+        timeAgo="Today at 4:45PM"
+        comments="HI"
+        avatar={faker.image.avatar()}
+      />
+    </ApprovalCard>
+    <ApprovalCard>
+      <CommentDetail
+        author="Alex"
+        timeAgo="Today at 2:00AM"
+        comments="Bye"
+        avatar={faker.image.avatar()}
+      />
+    </ApprovalCard>
+    <ApprovalCard>
+      <CommentDetail
+         author="Jane"
+         timeAgo="Today at 3:00PM"
+         comments="Goodnight"
+        avatar={faker.image.avatar()}
+       />
+    </ApprovalCard>
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.querySelector('#root'));
